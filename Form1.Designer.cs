ba305dcf6a97279203d06093c17a73f59244050f
Baubek Karibozin <baubekkaribozin@gmail.com>
2024-02-14 22:55:13 +0600

    Basic Calc creation

---
+++ Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace grav_force
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Button_Calc_Grav_force = new System.Windows.Forms.Button();
+            this.txtBox_Distance = new System.Windows.Forms.TextBox();
+            this.txtBox_mass_1 = new System.Windows.Forms.TextBox();
+            this.txtBox_mass_2 = new System.Windows.Forms.TextBox();
+            this.Mass_1 = new System.Windows.Forms.Label();
+            this.Mass_2 = new System.Windows.Forms.Label();
+            this.Distance = new System.Windows.Forms.Label();
+            this.Result = new System.Windows.Forms.Label();
+            this.txtBox_Result = new System.Windows.Forms.TextBox();
+            this.Instruction = new System.Windows.Forms.Label();
+            this.label_kilogramm_1 = new System.Windows.Forms.Label();
+            this.label_kilogramm_2 = new System.Windows.Forms.Label();
+            this.label_meter = new System.Windows.Forms.Label();
+            this.label_Newton = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Button_Calc_Grav_force
+            // 
+            this.Button_Calc_Grav_force.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Button_Calc_Grav_force.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.Button_Calc_Grav_force.Font = new System.Drawing.Font("Arial Rounded MT Bold", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Button_Calc_Grav_force.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Button_Calc_Grav_force.Location = new System.Drawing.Point(288, 496);
+            this.Button_Calc_Grav_force.Name = "Button_Calc_Grav_force";
+            this.Button_Calc_Grav_force.Size = new System.Drawing.Size(172, 45);
+            this.Button_Calc_Grav_force.TabIndex = 0;
+            this.Button_Calc_Grav_force.Text = "Calculate";
+            this.Button_Calc_Grav_force.UseVisualStyleBackColor = false;
+            this.Button_Calc_Grav_force.Click += new System.EventHandler(this.Button_Calc_Grav_force_Click);
+            // 
+            // txtBox_Distance
+            // 
+            this.txtBox_Distance.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtBox_Distance.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.txtBox_Distance.Font = new System.Drawing.Font("Arial Rounded MT Bold", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBox_Distance.Location = new System.Drawing.Point(288, 374);
+            this.txtBox_Distance.Name = "txtBox_Distance";
+            this.txtBox_Distance.Size = new System.Drawing.Size(252, 34);
+            this.txtBox_Distance.TabIndex = 1;
+            // 
+            // txtBox_mass_1
+            // 
+            this.txtBox_mass_1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtBox_mass_1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.txtBox_mass_1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBox_mass_1.Location = new System.Drawing.Point(288, 259);
+            this.txtBox_mass_1.Name = "txtBox_mass_1";
+            this.txtBox_mass_1.Size = new System.Drawing.Size(252, 34);
+            this.txtBox_mass_1.TabIndex = 2;
+            // 
+            // txtBox_mass_2
+            // 
+            this.txtBox_mass_2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtBox_mass_2.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.txtBox_mass_2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBox_mass_2.Location = new System.Drawing.Point(288, 319);
+            this.txtBox_mass_2.Name = "txtBox_mass_2";
+            this.txtBox_mass_2.Size = new System.Drawing.Size(252, 34);
+            this.txtBox_mass_2.TabIndex = 3;
+            // 
+            // Mass_1
+            // 
+            this.Mass_1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Mass_1.AutoSize = true;
+            this.Mass_1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Mass_1.Location = new System.Drawing.Point(15, 259);
+            this.Mass_1.Name = "Mass_1";
+            this.Mass_1.Size = new System.Drawing.Size(83, 54);
+            this.Mass_1.TabIndex = 5;
+            this.Mass_1.Text = "Mass1\r\n\r\n";
+            // 
+            // Mass_2
+            // 
+            this.Mass_2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Mass_2.AutoSize = true;
+            this.Mass_2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Mass_2.Location = new System.Drawing.Point(15, 319);
+            this.Mass_2.Name = "Mass_2";
+            this.Mass_2.Size = new System.Drawing.Size(83, 27);
+            this.Mass_2.TabIndex = 6;
+            this.Mass_2.Text = "Mass2";
+            // 
+            // Distance
+            // 
+            this.Distance.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Distance.AutoSize = true;
+            this.Distance.Font = new System.Drawing.Font("Arial Rounded MT Bold", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Distance.Location = new System.Drawing.Point(15, 374);
+            this.Distance.Name = "Distance";
+            this.Distance.Size = new System.Drawing.Size(111, 27);
+            this.Distance.TabIndex = 7;
+            this.Distance.Text = "Distance";
+            // 
+            // Result
+            // 
+            this.Result.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Result.AutoSize = true;
+            this.Result.Font = new System.Drawing.Font("Arial Rounded MT Bold", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Result.Location = new System.Drawing.Point(15, 440);
+            this.Result.Name = "Result";
+            this.Result.Size = new System.Drawing.Size(228, 27);
+            this.Result.TabIndex = 8;
+            this.Result.Text = "Gravitational Force";
+            // 
+            // txtBox_Result
+            // 
+            this.txtBox_Result.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtBox_Result.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.txtBox_Result.Font = new System.Drawing.Font("Arial Rounded MT Bold", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBox_Result.Location = new System.Drawing.Point(288, 440);
+            this.txtBox_Result.Name = "txtBox_Result";
+            this.txtBox_Result.Size = new System.Drawing.Size(252, 34);
+            this.txtBox_Result.TabIndex = 9;
+            // 
+            // Instruction
+            // 
+            this.Instruction.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Instruction.AutoSize = true;
+            this.Instruction.Font = new System.Drawing.Font("Yu Gothic UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Instruction.Location = new System.Drawing.Point(-2, 35);
+            this.Instruction.Name = "Instruction";
+            this.Instruction.Size = new System.Drawing.Size(924, 93);
+            this.Instruction.TabIndex = 10;
+            this.Instruction.Text = "Введите число с помощью цифр и \",\"(запятая) без пробелов. \r\nУбедитесь что значени" +
+    "я даны в СИ.\r\nЗапись экспоненты производится через \"E\", например 1,1E2 = 110, то" +
+    " есть 1,1 * 10^2.\r\n";
+            // 
+            // label_kilogramm_1
+            // 
+            this.label_kilogramm_1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label_kilogramm_1.AutoSize = true;
+            this.label_kilogramm_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_kilogramm_1.Location = new System.Drawing.Point(545, 264);
+            this.label_kilogramm_1.Name = "label_kilogramm_1";
+            this.label_kilogramm_1.Size = new System.Drawing.Size(38, 29);
+            this.label_kilogramm_1.TabIndex = 11;
+            this.label_kilogramm_1.Text = "кг";
+            // 
+            // label_kilogramm_2
+            // 
+            this.label_kilogramm_2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label_kilogramm_2.AutoSize = true;
+            this.label_kilogramm_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_kilogramm_2.Location = new System.Drawing.Point(545, 324);
+            this.label_kilogramm_2.Name = "label_kilogramm_2";
+            this.label_kilogramm_2.Size = new System.Drawing.Size(38, 29);
+            this.label_kilogramm_2.TabIndex = 12;
+            this.label_kilogramm_2.Text = "кг";
+            // 
+            // label_meter
+            // 
+            this.label_meter.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label_meter.AutoSize = true;
+            this.label_meter.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_meter.Location = new System.Drawing.Point(545, 379);
+            this.label_meter.Name = "label_meter";
+            this.label_meter.Size = new System.Drawing.Size(34, 29);
+            this.label_meter.TabIndex = 13;
+            this.label_meter.Text = "м";
+            // 
+            // label_Newton
+            // 
+            this.label_Newton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label_Newton.AutoSize = true;
+            this.label_Newton.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_Newton.Location = new System.Drawing.Point(545, 445);
+            this.label_Newton.Name = "label_Newton";
+            this.label_Newton.Size = new System.Drawing.Size(31, 29);
+            this.label_Newton.TabIndex = 14;
+            this.label_Newton.Text = "Н";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.AutoSize = true;
+            this.groupBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.groupBox1.Controls.Add(this.Instruction);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox1.Location = new System.Drawing.Point(54, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1075, 158);
+            this.groupBox1.TabIndex = 15;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Инструкция";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Image = global::sau3.Properties.Resources.newton_law_gravitation_gravity_gravitational_force;
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(606, 259);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(523, 215);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(1229, 552);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label_Newton);
+            this.Controls.Add(this.label_meter);
+            this.Controls.Add(this.label_kilogramm_2);
+            this.Controls.Add(this.label_kilogramm_1);
+            this.Controls.Add(this.txtBox_Result);
+            this.Controls.Add(this.Result);
+            this.Controls.Add(this.Distance);
+            this.Controls.Add(this.Mass_2);
+            this.Controls.Add(this.Mass_1);
+            this.Controls.Add(this.txtBox_mass_2);
+            this.Controls.Add(this.txtBox_mass_1);
+            this.Controls.Add(this.txtBox_Distance);
+            this.Controls.Add(this.Button_Calc_Grav_force);
+            this.MinimumSize = new System.Drawing.Size(1247, 599);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Gravitational Force";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Button_Calc_Grav_force;
+        private System.Windows.Forms.TextBox txtBox_Distance;
+        private System.Windows.Forms.TextBox txtBox_mass_1;
+        private System.Windows.Forms.TextBox txtBox_mass_2;
+        private System.Windows.Forms.Label Mass_1;
+        private System.Windows.Forms.Label Mass_2;
+        private System.Windows.Forms.Label Distance;
+        private System.Windows.Forms.Label Result;
+        private System.Windows.Forms.TextBox txtBox_Result;
+        private System.Windows.Forms.Label Instruction;
+        private System.Windows.Forms.Label label_kilogramm_1;
+        private System.Windows.Forms.Label label_kilogramm_2;
+        private System.Windows.Forms.Label label_meter;
+        private System.Windows.Forms.Label label_Newton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace grav_force
+{
+    public partial class Form1 : Form
+    {
+        public Form1() => InitializeComponent();
+        private void Calc(double mass1, double mass2, double distance)//Нахождение силы притяжение
+        {
+            double result = 6.6743E-11 * mass1 * mass2 / (distance * distance);
+            txtBox_Result.Text = result.ToString();
+
+        }
+
+
+        private void Button_Calc_Grav_force_Click(object sender, EventArgs e)
+        {
+            bool convertion_mass1 = double.TryParse(txtBox_mass_1.Text, out double mass1);
+            bool convertion_mass2 = double.TryParse(txtBox_mass_2.Text, out double mass2);
+            bool convertion_distance = double.TryParse(txtBox_Distance.Text, out double distance);//Проверяет, возможно ли проеброзование. Преоброзовывает если возможно
+
+            if (convertion_mass1 && convertion_mass2 && convertion_distance)
+                Calc(mass1, mass2, distance);
+
+            else
+                MessageBox.Show("Ошибка. Вводите числа следуя инструкциям!");
+
+            if (distance == 0) MessageBox.Show("Ошибка. Дистанция равна 0!");
+
+        }
+    }
+}
